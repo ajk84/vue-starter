cc7e12effe1d9ac313bb3047873be50c8e80150e
Alina Kramp <alinakramp@Alinas-MacBook-Pro.local>
2020-05-23 18:37:49 -0500

    Deploy

---
+++ precache-manifest.33c4f889fa924e4ab09f58ecc1f36ab5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0732b29eac4e6b8f269f",
+    "url": "/chacosworld/css/about.15ebb9dc.css"
+  },
+  {
+    "revision": "c92e2b763a289b724803",
+    "url": "/chacosworld/css/app.5c6d1a1f.css"
+  },
+  {
+    "revision": "db95d67c79a3b7f6b2b5",
+    "url": "/chacosworld/css/chunk-453802f6.41e8ad32.css"
+  },
+  {
+    "revision": "1ce3742336f5061273f2",
+    "url": "/chacosworld/css/chunk-vendors.4898d00d.css"
+  },
+  {
+    "revision": "907642cf3ac9df4844259cacc86bd21f",
+    "url": "/chacosworld/index.html"
+  },
+  {
+    "revision": "0732b29eac4e6b8f269f",
+    "url": "/chacosworld/js/about.9b7b8411.js"
+  },
+  {
+    "revision": "c92e2b763a289b724803",
+    "url": "/chacosworld/js/app.829d13ea.js"
+  },
+  {
+    "revision": "db95d67c79a3b7f6b2b5",
+    "url": "/chacosworld/js/chunk-453802f6.3f68955b.js"
+  },
+  {
+    "revision": "1ce3742336f5061273f2",
+    "url": "/chacosworld/js/chunk-vendors.fe9e0f94.js"
+  },
+  {
+    "revision": "18856f89300cb9d5fe9740e2f0bb81cb",
+    "url": "/chacosworld/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chacosworld/robots.txt"
+  }
+]);
